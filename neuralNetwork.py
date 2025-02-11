b58863b2bbedc9036c8173b15f57671c308c5542
Cordagr <113870656+Cordagr@users.noreply.github.com>
2025-02-11 09:52:58 -0600

    Update neuralNetwork.py
    Implement layer output and non -linearity

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -8,6 +8,10 @@ train_df = df_sample(frac=.75,, random_state = 4)
 # Dropping training data from orig training set
 val_df = df.drop(train_df.index)
 
+# Defining 
+# hidden layerswilll transform data into weights and biases
+# output_layer will produce network's final prediction
+
 def neural_network_model(train_df):
       hidden_layer_1  = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1])),
                          'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))}
@@ -20,6 +24,10 @@ def neural_network_model(train_df):
       
       output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
                          'biases': tf.Variable(tf.random_normal([n_classes]))}
+# Compute first layer output 
+l1 = l1.add(tf.matmul(data,hidden_layer_1['weights']), hidden_layer_1)
+# Apply ReLU
+l1 = tf.nn.relu(l1)
 
 
 
