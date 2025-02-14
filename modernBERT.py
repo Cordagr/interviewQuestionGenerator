3e1857733eba70632681ba79ecdfba7e903b6f50
Cordagr <113870656+Cordagr@users.noreply.github.com>
2025-02-13 18:23:17 -0600

    Update modernBERT.py
    Importing rest of needed libraries

--- modernBERT.py
+++ modernBERT.py
@@ -1,3 +1,21 @@
 import torch
 from transformers import pipeline
 from pprint import pprint
+import numpy as np 
+import pandas as pd
+import gc
+from datasets import load_dataset
+from sklearn.metrics import f1_score
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    DataCollatorWithPadding,
+    TrainingArguments,
+    Trainer,
+    TrainerCallback,
+)
+
+from sklearn.metrics import matthews_corrcoef, accuracy_score, f1_score
+from scipy.stats import pearsonr, spearmanr
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
